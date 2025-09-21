6116944f4eaf30a55a4c1836ac14efc723a0362b
Mohammadi <ms.mohammadi69@gmail.com>
2025-09-21 15:26:35 +0330

    Add background image

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,23 @@ function App() {
   const [editingTask, setEditingTask] = useState<Task | null>(null);
 
   return (
-    <div className="min-h-[80vh] md:min-h-screen bg-gray-500/50 flex items-center justify-center md:justify-start md:p-4">
-      <div className="bg-white shadow-lg rounded-xl p-6 w-full max-w-lg">
-        <h1 className="text-xl md:text-2xl font-bold mb-4 text-center">
-          📝 To-Do App List Manager
-        </h1>
+    <div
+      className="bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/bg.png')" }}
+    >
+      <div className="min-h-[80vh] md:min-h-screen bg-gray-500/50 flex items-center justify-center md:justify-start md:p-4">
+        <div className="bg-white shadow-lg rounded-xl p-6 w-full max-w-lg">
+          <h1 className="text-xl md:text-2xl font-bold mb-4 text-center">
+            📝 To-Do App List Manager
+          </h1>
 
-        <TaskForm
-          editingTask={editingTask}
-          onFinishEdit={() => setEditingTask(null)}
-        />
+          <TaskForm
+            editingTask={editingTask}
+            onFinishEdit={() => setEditingTask(null)}
+          />
 
-        <TaskList onEdit={(task) => setEditingTask(task)} />
+          <TaskList onEdit={(task) => setEditingTask(task)} />
+        </div>
       </div>
     </div>
   );
